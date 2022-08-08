3d9834b9002097a6887adee3234b1823215bacfd
RhysHuo <RhysHuo_1120272267@163.com>
2022-08-08 13:37:07 +0100

    Update control_host.sh

--- control_host.sh
+++ control_host.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/RhysHuo/gemm_Vitis_Version.git
-cd gemm_Vitis_Version
+git clone https://github.com/RhysHuo/gemm_Vitis_Updata.git
+cd gemm_Vitis_Updata
 cp host.cpp ..
 cd ..
-rm -rf gemm_Vitis_Version
+rm -rf gemm_Vitis_Updata
 g++ -g -std=c++14 -I$XILINX_XRT/include -L${XILINX_XRT}/lib/ -I/mnt/scratch/rhyhuo/HLS_arbitrary_Precision_Types/include -o host.exe host.cpp -lOpenCL -pthread -lrt -lstdc++
