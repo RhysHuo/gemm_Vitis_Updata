0a8a51f6f7feb2d48b1ae6ff1ad28945a707d368
RhysHuo <RhysHuo_1120272267@163.com>
2022-08-08 13:43:44 +0100

    Update kernelMatrixmult.cpp

--- kernelMatrixmult.cpp
+++ kernelMatrixmult.cpp
@@ -119,12 +119,12 @@ void mmult_top(ap_uint<2> ternary, int N, int M, int P, DTYPE* A, DTYPE* B, DTYP
             mmult_accel(ternary, M, A_accel, B_accel, C_accel);
 
             for (int i = 0; i < C_HEIGHT_BLOCK; i++) {
-                for (int j = 0; j < C_WIDTH_BLOCK; j++) 
+                for (int j = 0; j < C_WIDTH_BLOCK; j++) {
 					#pragma HLS UNROLL
                     C[(i+A_index*A_HEIGHT_BLOCK)*P+j+B_index*B_WIDTH_BLOCK] = C_accel[i*C_WIDTH_BLOCK+j];
                     //std::cout << "C is " << C[(i+A_index*A_HEIGHT_BLOCK)*P+j+B_index*B_WIDTH_BLOCK] << std::endl;
-                }
+				}
             }
-	    }
+        }
 	}
 }
